8171759e181828854a0a3ef2d6c05948ca078798
Rubel Mahmud <vxlrubel@gmail.com>
2026-05-14 04:00:37 +0600

    feat: register DeleteConfirmation component in App.vue

--- dashboard-and-store/src/App.vue
+++ dashboard-and-store/src/App.vue
@@ -3,6 +3,7 @@ import { RouterView } from 'vue-router'
 import MediaBox from '@/components/media-box/MediaBox.vue'
 import ToastMessage from '@/components/ToastMessage.vue'
 import LeaveConfirmation from '@/components/LeaveConfirmation.vue'
+import DeleteConfirmation from '@/components/DeleteConfirmation.vue'
 import { useToastStore } from '@/stores/toastStore'
 
 const toastStore = useToastStore()
@@ -12,6 +13,7 @@ const toastStore = useToastStore()
   <RouterView />
   <MediaBox />
   <LeaveConfirmation />
+  <DeleteConfirmation title="Move to Trash" shortDescription="Are you sure you want to move this item to trash?" />
   <ToastMessage
     v-if="toastStore.visible"
     :status="toastStore.status"

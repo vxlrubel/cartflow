cf57a2bba4c44b0dccf076fc7e716e3aec315eca
Rubel Mahmud <vxlrubel@gmail.com>
2026-05-14 23:40:33 +0600

    feat: add EnvelopeIcon component with customizable size

---
+++ dashboard-and-store/src/components/icons/EnvelopeIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg :width="size" :height="size" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M21.25 6.23398V17.25C21.25 18.0784 20.5784 18.75 19.75 18.75H4.25C3.42157 18.75 2.75 18.0784 2.75 17.25V6.23398M3.73592 5.25H20.2644C20.8088 5.25 21.2501 5.69124 21.2501 6.23558C21.2502 6.5579 21.0926 6.85988 20.8282 7.04424L12.8583 12.6019C12.3428 12.9614 11.6579 12.9614 11.1424 12.6019L3.17217 7.04405C2.90794 6.8598 2.75043 6.55805 2.75032 6.23592C2.75014 5.69146 3.19146 5.25 3.73592 5.25Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+</template>
+
+<script setup>
+defineProps({
+  size: {
+    type: [String, Number],
+    default: 24,
+  },
+})
+</script>
